2db2fa694baa1c1ca0cfaf902537d031087ccc19
studenti <studenti@LAB2-PC-10>
2018-01-22 09:29:52 +0100

    ho modificato branches sviluppo

--- Array/Program.cs
+++ Array/Program.cs
@@ -10,7 +10,7 @@ namespace Array
     {
         static void Main(string[] args)
         {
-            int A = 10;
+            int A = 15;
             int[] Numeri = new int[A];
 
             for (int i = 0; i < A; i++)
